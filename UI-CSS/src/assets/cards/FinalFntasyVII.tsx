490509a43db31a8b308121b74259a0f5c6a7c261
Brendon <brendonpjon@gmail.com>
2023-05-10 13:19:13 +0100

    Added more cards and sorted deployment

---
+++ UI-CSS/src/assets/cards/FinalFntasyVII.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function FinalFntasyVII() {
+  return (
+    <div className='card-box ff7-card'>
+        <div className='ff7-card-cloud'>
+            <div className='ff7-test'></div>
+            <img src="cloud.webp" alt="" />
+        </div>
+        <div className='ff7-card-seph'>
+           <div className='ff7-test'></div>
+            <img src="seph.webp" alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default FinalFntasyVII
